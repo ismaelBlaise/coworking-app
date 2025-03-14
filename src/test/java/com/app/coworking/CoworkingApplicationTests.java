61fc006a303c54808599684ccf9c64b986fef598
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2025-03-14 09:13:37 +0300

    initilisation du projet

---
+++ src/test/java/com/app/coworking/CoworkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.coworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
